a6851458d6161f620e85d2742fe93485ae048aad
Alexandre Girard <git@alexgirard.com>
2011-11-09 16:45:29 +0100

    invert JOY_UP and JOY_DOWN

--- Marlin/lcd.h
+++ Marlin/lcd.h
@@ -4,8 +4,8 @@
   #define STATUSTIMEOUT 15000
 
   #define JOY_RIGHT 0
-  #define JOY_DOWN  1
-  #define JOY_UP    2
+  #define JOY_UP    1
+  #define JOY_DOWN  2
   #define JOY_LEFT  3
   #define JOY_OK    4
 
